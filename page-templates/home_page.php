125dc7abd4610949e2acd4312015be553bc8a4be
berniesecretary <42022969+berniesecretary@users.noreply.github.com>
2018-09-16 15:47:32 -0500

    Add files via upload

---
+++ page-templates/home_page.php
@@ -0,0 +1,137 @@
+<?php
+/**
+* Template Name:home_page
+* @package steemtemplates
+*/
+get_header(); ?>
+<div id="stt_content_and_sidebar_container">
+<div id="stt_content">
+<div id="stt_html_content_margin">
+<div class="remove_collapsing_margins"></div>
+<?php if (steemtemplates_theme_option("ttr_page_breadcrumb")):?>
+<?php steemtemplates_wordpress_breadcrumbs(); ?>
+<?php endif; ?>
+<?php
+if( is_active_sidebar( 'contenttopcolumn1'  ) || is_active_sidebar( 'contenttopcolumn2'  ) || is_active_sidebar( 'contenttopcolumn3'  ) || is_active_sidebar( 'contenttopcolumn4'  )):
+?>
+<div class="stt_topcolumn_widget_container">
+<div class="contenttopcolumn0">
+<?php if ( is_active_sidebar('contenttopcolumn1') ) : ?>
+<div class="cell1 col-lg-3 col-md-6 col-sm-6  col-xs-12">
+<div class="topcolumn1">
+<?php steemtemplates_theme_dynamic_sidebar( 'CAWidgetArea00'); ?>
+</div>
+</div>
+<?php else: ?>
+<div class="cell1 col-lg-3 col-md-6 col-sm-6  col-xs-12transparent">
+&nbsp;
+</div>
+<?php endif; ?>
+<div class=" visible-xs-block" style="clear:both;"></div>
+<?php if ( is_active_sidebar('contenttopcolumn2') ) : ?>
+<div class="cell2 col-lg-3 col-md-6 col-sm-6  col-xs-12">
+<div class="topcolumn2">
+<?php steemtemplates_theme_dynamic_sidebar( 'CAWidgetArea01'); ?>
+</div>
+</div>
+<?php else: ?>
+<div class="cell2 col-lg-3 col-md-6 col-sm-6  col-xs-12transparent">
+&nbsp;
+</div>
+<?php endif; ?>
+<div class=" visible-sm-block visible-md-block visible-xs-block" style="clear:both;"></div>
+<?php if ( is_active_sidebar('contenttopcolumn3') ) : ?>
+<div class="cell3 col-lg-3 col-md-6 col-sm-6  col-xs-12">
+<div class="topcolumn3">
+<?php steemtemplates_theme_dynamic_sidebar( 'CAWidgetArea02'); ?>
+</div>
+</div>
+<?php else: ?>
+<div class="cell3 col-lg-3 col-md-6 col-sm-6  col-xs-12transparent">
+&nbsp;
+</div>
+<?php endif; ?>
+<div class=" visible-xs-block" style="clear:both;"></div>
+<?php if ( is_active_sidebar('contenttopcolumn4') ) : ?>
+<div class="cell4 col-lg-3 col-md-6 col-sm-6  col-xs-12">
+<div class="topcolumn4">
+<?php steemtemplates_theme_dynamic_sidebar( 'CAWidgetArea03'); ?>
+</div>
+</div>
+<?php else: ?>
+<div class="cell4 col-lg-3 col-md-6 col-sm-6  col-xs-12transparent">
+&nbsp;
+</div>
+<?php endif; ?>
+<div class=" visible-lg-block visible-sm-block visible-md-block visible-xs-block" style="clear:both;"></div>
+</div>
+</div>
+<div style="clear: both;"></div>
+<?php endif; ?>
+<?php while ( have_posts() ) : the_post(); ?>
+<?php get_template_part( 'content', 'page' ); ?>
+<?php comments_template( '', true ); ?>
+<?php endwhile; // end of the loop. ?>
+<?php
+if( is_active_sidebar( 'contentbottomcolumn1'  ) || is_active_sidebar( 'contentbottomcolumn2'  ) || is_active_sidebar( 'contentbottomcolumn3'  ) || is_active_sidebar( 'contentbottomcolumn4'  )):
+?>
+<div class="stt_bottomcolumn_widget_container">
+<div class="contentbottomcolumn0">
+<?php if ( is_active_sidebar('contentbottomcolumn1') ) : ?>
+<div class="cell1 col-lg-3 col-md-6 col-sm-6  col-xs-12">
+<div class="bottomcolumn1">
+<?php steemtemplates_theme_dynamic_sidebar( 'CBWidgetArea00'); ?>
+</div>
+</div>
+<?php else: ?>
+<div class="cell1 col-lg-3 col-md-6 col-sm-6  col-xs-12transparent">
+&nbsp;
+</div>
+<?php endif; ?>
+<div class=" visible-xs-block" style="clear:both;"></div>
+<?php if ( is_active_sidebar('contentbottomcolumn2') ) : ?>
+<div class="cell2 col-lg-3 col-md-6 col-sm-6  col-xs-12">
+<div class="bottomcolumn2">
+<?php steemtemplates_theme_dynamic_sidebar( 'CBWidgetArea01'); ?>
+</div>
+</div>
+<?php else: ?>
+<div class="cell2 col-lg-3 col-md-6 col-sm-6  col-xs-12transparent">
+&nbsp;
+</div>
+<?php endif; ?>
+<div class=" visible-sm-block visible-md-block visible-xs-block" style="clear:both;"></div>
+<?php if ( is_active_sidebar('contentbottomcolumn3') ) : ?>
+<div class="cell3 col-lg-3 col-md-6 col-sm-6  col-xs-12">
+<div class="bottomcolumn3">
+<?php steemtemplates_theme_dynamic_sidebar( 'CBWidgetArea02'); ?>
+</div>
+</div>
+<?php else: ?>
+<div class="cell3 col-lg-3 col-md-6 col-sm-6  col-xs-12transparent">
+&nbsp;
+</div>
+<?php endif; ?>
+<div class=" visible-xs-block" style="clear:both;"></div>
+<?php if ( is_active_sidebar('contentbottomcolumn4') ) : ?>
+<div class="cell4 col-lg-3 col-md-6 col-sm-6  col-xs-12">
+<div class="bottomcolumn4">
+<?php steemtemplates_theme_dynamic_sidebar( 'CBWidgetArea03'); ?>
+</div>
+</div>
+<?php else: ?>
+<div class="cell4 col-lg-3 col-md-6 col-sm-6  col-xs-12transparent">
+&nbsp;
+</div>
+<?php endif; ?>
+<div class=" visible-lg-block visible-sm-block visible-md-block visible-xs-block" style="clear:both;"></div>
+</div>
+</div>
+<div style="clear: both;"></div>
+<?php endif; ?>
+<div class="remove_collapsing_margins"></div>
+</div>
+</div>
+<div style="clear: both;"></div>
+</div>
+<?php get_footer(); ?>
